15533c71dadf89ac6dc5374e810121bc2ab0b8dd
J.R. Mash <2574997+jrmash@users.noreply.github.com>
2021-11-27 23:27:00 -0800

    Add a unit test

---
+++ test/unit_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"testing"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestUnitTest(t *testing.T) {
+	t.Parallel()
+	
+	terraformOptions := &terraform.Options{
+		TerraformDir: "unit-test",
+	}
+	
+	defer terraform.Destroy(t, terraformOptions)
+	
+	terraform.InitAndPlan(t, terraformOptions)
+	terraform.ApplyAndIdempotent(t, terraformOptions)
+}
