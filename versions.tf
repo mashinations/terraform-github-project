ea887e1f76b488497a4ce625db0fa4a289e59c1c
J.R. Mash <2574997+jrmash@users.noreply.github.com>
2021-11-29 16:07:50 -0800

    Specify the GitHub provider version

--- versions.tf
+++ versions.tf
@@ -1,4 +1,10 @@
 terraform {
   experiments      = [module_variable_optional_attrs]
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "~> 4.18"
+    }
+  }
   required_version = "~> 1.0"
 }
