0db46f125d3cadc9f0e3aec79e25a016453ac624
J.R. Mash <2574997+jrmash@users.noreply.github.com>
2021-11-30 16:57:51 -0800

    Require v4.18.2 of the GitHub provider

--- versions.tf
+++ versions.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     github = {
       source = "integrations/github"
-      version = "~> 4.18"
+      version = "~> 4.18.2"
     }
   }
   required_version = "~> 1.0"
